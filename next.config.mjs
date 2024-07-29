abdc6b388cade8f7fbb92b2d0c0085bec2a22e7a
sh-oon <jshyh1@naver.com>
2024-07-30 00:15:19 +0900

    chore / path rewrite 제거

--- next.config.mjs
+++ next.config.mjs
@@ -28,10 +28,7 @@ const nextConfig = {
   async rewrites() {
     return {
       beforeFiles: [
-        {
-          source: '/api/:path*',
-          destination: `${process.env.NEXT_PUBLIC_API_URL}/api/:path*`,
-        },
+
       ],
     }
   },
