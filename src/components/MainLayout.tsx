70e81212184cb614dd486e624fbd657fbd62777b
sh-oon <jshyh1@naver.com>
2024-09-21 00:01:15 +0900

    style

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,14 +1,19 @@
 import styled from '@emotion/styled'
 import React, { PropsWithChildren } from 'react';
+import { vars } from '@ui-tokens';
 
 function MainLayout({ children }: PropsWithChildren<{}>) {
-  return <StyledMain className='min-h-dvh pt-header bg-grey-50 dark:bg-black relative'>{children}</StyledMain>;
+  return <StyledMain>{children}</StyledMain>;
 }
 
 const StyledMain = styled.main`
   height: 100dvh;
   padding-top: 80px;
   position: relative;
+  
+  @media (prefers-color-scheme: dark) {
+    background-color: ${vars.$semantic.color.background.dark};
+  }
 `
 
 export default MainLayout;
